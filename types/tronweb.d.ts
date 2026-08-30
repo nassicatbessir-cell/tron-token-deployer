71d4f80800791c65e74071e2a209839a767967fe
abab azriy <nassicatbessir@gmail.com>
2026-08-31 00:34:12 +0330

    fix: type TronLink wallet events

--- types/tronweb.d.ts
+++ types/tronweb.d.ts
@@ -52,5 +52,7 @@ interface Window {
   tronLink?: {
     tronWeb?: TronWebLike;
     request?: (args: TronLinkRequestArgs) => Promise<unknown>;
+    on?: (event: string, listener: (...args: unknown[]) => void) => void;
+    removeListener?: (event: string, listener: (...args: unknown[]) => void) => void;
   };
 }
